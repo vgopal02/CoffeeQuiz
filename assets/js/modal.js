e4913a41f19b04265471142477d04e2c62e2b608
vgopal02 <rajeev.vidya@outlook.com>
2021-07-27 13:47:15 +0000

    Add eventListerner for open and close modal and to close modal when clicking anywhere outside it

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -5,3 +5,9 @@ var modalBtn = document.getElementById("modalBtn");
 //Get close button
 var closeBtn = document.getElementsByClassName("closeBtn")[0];
 
+//Liosten for open click
+modalBtn.addEventListener("click", openModal);
+//Listen for close click
+closeBtn.addEventListener("click", closeModal);
+//Listen for outside click
+window.addEventListener("click", outsideClick);
