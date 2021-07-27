501f828d16c063980b2b6988c7ae18a745b7ecb9
vgopal02 <rajeev.vidya@outlook.com>
2021-07-27 13:45:21 +0000

    Add variable to create open and close modal

---
+++ assets/js/modal.js
@@ -0,0 +1,7 @@
+//Get Modal element
+var modal = document.getElementById("simpleModal");
+//Get open modal button
+var modalBtn = document.getElementById("modalBtn");
+//Get close button
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
