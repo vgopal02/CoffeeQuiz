4b86f68267450e24c8b1cf8766ca571255feb366
vgopal02 <rajeev.vidya@outlook.com>
2021-07-28 07:49:14 +0000

    Add get new question and choice option for each question

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -200,3 +200,42 @@ startGame = () => {
     availableQuesions = [...questions];
     getNewQuestion();
   };
+
+  getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+      localStorage.setItem("mostRecentScore", score);
+      //go to the end page
+      return window.location.assign("/conclude.html");
+    }
+    questionCounter++;
+    questionCounterText.innerText=questionCounter + "/" + MAX_QUESTIONS ;
+  
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+  
+    choices.forEach(choice => {
+      const number = choice.dataset["number"];
+      choice.innerText = currentQuestion["choice" + number];
+    });
+  
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+  };
+  
+  choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+      if (!acceptingAnswers) return;
+  
+      acceptingAnswers = false;
+      const selectedChoice = e.target;
+      const selectedAnswer = selectedChoice.dataset["number"];
+  
+      const classToApply =
+        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+  
+        if(classToApply === "correct") {
+          incrementScore(CORRECT_BONUS);
+        }
+  
+      selectedChoice.parentElement.classList.add(classToApply);
