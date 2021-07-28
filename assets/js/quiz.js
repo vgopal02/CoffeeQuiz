44662de732c6778e708f081bb765d7a8110b6ee5
vgopal02 <rajeev.vidya@outlook.com>
2021-07-28 07:48:05 +0000

    Add start game in quiz.js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -193,3 +193,10 @@ let questions = [
 //CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
+    getNewQuestion();
+  };
