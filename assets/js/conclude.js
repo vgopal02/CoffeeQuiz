cfb11da65e145c4a335aad489d2c5c948f94c3a6
vgopal02 <rajeev.vidya@outlook.com>
2021-07-29 06:18:44 +0000

    Add const - username save button final score and most recent score in conclude.js

---
+++ assets/js/conclude.js
@@ -0,0 +1,4 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
