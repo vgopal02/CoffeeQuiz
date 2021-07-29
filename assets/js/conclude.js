f0a43ac7eb4a5a9d53e42fa9e37041e45bfe9f3f
vgopal02 <rajeev.vidya@outlook.com>
2021-07-29 06:19:27 +0000

    Add max 5 highscores to be saved

--- assets/js/conclude.js
+++ assets/js/conclude.js
@@ -2,3 +2,7 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
