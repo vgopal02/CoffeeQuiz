a51bd1b044b99138c203641eb77c66b3602d3f11
vgopal02 <rajeev.vidya@outlook.com>
2021-07-29 06:30:16 +0000

    Add headers to conclude.js

--- assets/js/conclude.js
+++ assets/js/conclude.js
@@ -1,3 +1,4 @@
+//Register user final score with option to save
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
@@ -5,10 +6,27 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
+//Maximum of 5 high scores can be saved and stored
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
+//Save button disabled until user adds text to the input 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
+
+saveHighScore = (e) => {
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+};
