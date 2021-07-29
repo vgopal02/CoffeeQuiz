c747da6260becd8a5ca8dcac57783f6d62772368
vgopal02 <rajeev.vidya@outlook.com>
2021-07-29 06:20:46 +0000

    Disable save button until user input name

--- assets/js/conclude.js
+++ assets/js/conclude.js
@@ -6,3 +6,9 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
