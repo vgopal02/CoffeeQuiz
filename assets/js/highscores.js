38094af15ec7b520900fd5c18c10228aec111ab4
vgopal02 <rajeev.vidya@outlook.com>
2021-08-04 21:01:57 +0000

    Add description headers in highscores.js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,6 +1,11 @@
+//jshint esversion :6
+// Code from Build a Quiz App
+
+//Get highscore from local storage
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+//Convert the highscores into a list
 highScoresList.innerHTML = highScores
   .map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
